f8594e632dcce19d2e2000e6e1c18046bb1c100a
october03 <season_autumn@kakao.com>
2024-02-10 11:08:13 +0900

    Initial Commit

---
+++ src/main/kotlin/com/october03/cafe/kiosk/product/KioskProductServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.october03.cafe.kiosk.product
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KioskProductServiceApplication
+
+fun main(args: Array<String>) {
+  runApplication<KioskProductServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/october03/cafe/kiosk/product/KioskProductServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.october03.cafe.kiosk.product
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KioskProductServiceApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
