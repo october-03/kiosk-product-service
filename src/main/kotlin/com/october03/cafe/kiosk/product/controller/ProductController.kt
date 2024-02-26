0091c97025aaf5a1402bc6c51deec4cffee21768
october03 <season_autumn@kakao.com>
2024-02-26 16:48:13 +0900

    상품 조회 API 개발

--- src/main/kotlin/com/october03/cafe/kiosk/product/controller/ProductController.kt
+++ src/main/kotlin/com/october03/cafe/kiosk/product/controller/ProductController.kt
@@ -1,8 +1,11 @@
 package com.october03.cafe.kiosk.product.controller
 
 import com.october03.cafe.kiosk.product.dto.CreateProductDto
+import com.october03.cafe.kiosk.product.dto.ProductCategory
 import com.october03.cafe.kiosk.product.repository.Product
 import com.october03.cafe.kiosk.product.service.ProductService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -12,5 +15,20 @@ class ProductController(private val productService: ProductService) {
   @PostMapping("/register")
   fun register(@RequestBody req: CreateProductDto): Product {
     return productService.createProduct(req)
+  }
+
+  @GetMapping("/{id}")
+  fun getProductById(@PathVariable id: Long): Product? {
+    return productService.getProductById(id)
+  }
+
+  @GetMapping("/category/{category}")
+  fun getProductByCategory(@PathVariable category: ProductCategory): List<Product>? {
+    return productService.getProductsByCategory(category)
+  }
+
+  @GetMapping("/all")
+  fun getAllProducts(): List<Product>? {
+    return productService.getAllProducts()
   }
 }
--- src/main/kotlin/com/october03/cafe/kiosk/product/service/ProductService.kt
+++ src/main/kotlin/com/october03/cafe/kiosk/product/service/ProductService.kt
@@ -1,6 +1,7 @@
 package com.october03.cafe.kiosk.product.service
 
 import com.october03.cafe.kiosk.product.dto.CreateProductDto
+import com.october03.cafe.kiosk.product.dto.ProductCategory
 import com.october03.cafe.kiosk.product.repository.Product
 import com.october03.cafe.kiosk.product.repository.ProductRepository
 import org.springframework.stereotype.Service
@@ -18,5 +19,17 @@ class ProductService(private val productRepository: ProductRepository) {
     productRepository.save(newProduct)
 
     return newProduct
+  }
+
+  fun getProductById(id: Long): Product? {
+    return productRepository.findById(id).orElse(null)
+  }
+
+  fun getProductsByCategory(category: ProductCategory): List<Product>? {
+    return productRepository.findByCategory(category)
+  }
+
+  fun getAllProducts(): List<Product>? {
+    return productRepository.findAll()
   }
 }
